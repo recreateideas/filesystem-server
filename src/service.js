2d3e0a75154c9ac630a95b645e6692fad00f2a30
Claudio De Angelis <claudio.deangelis@cloud-iq.com>
2018-12-16 21:03:02 +1100

    fixed console log

--- src/service.js
+++ src/service.js
@@ -15,27 +15,29 @@ chalk.green(
         if (err) {return;}
         console.log(`
 _____________________________________________________________________________________
-                                WELCOME TO
+                                  WELCOME TO
         `);
         console.log(chalk.green(data));
 
         console.log(`
-            ##############################################
+            #######################################################
                                      
-                Listening on localhost, port: ${port}     
-                ---------------------------------       
-                We're ready to serve your filesystem.        
-                to access a file via API call
-                send its uri-encoded, absolute path to 
-                    ${chalk.red(`http://localhost:`)}${chalk.red(port)}${chalk.red(`/files/`)}        
+                      Listening on localhost, port: ${port}     
+                      ---------------------------------\n       
+                      We're ready to serve your filesystem.        
+                          to access a file via API call
+                    send its absolute path, uri-encoded, to:
+                          ${chalk.red(`http://localhost:`)}${chalk.red(port)}${chalk.red(`/files/`)}\n
+                      ---------------------------------
+          ${chalk.green(`like this->`)} ${chalk.underline(`http://localhost:${port}/files/%2Fhome%2Ffile%2Ejs`)}    
                                           
-            ##############################################
-                \u00A9 ${chalk.blue(`ReCreateIdeas`)} - find repo on GitHub\n
+         ###############################################################
+                      \u00A9 ${chalk.blue(`ReCreateIdeas`)} - fork us on GitHub\n
 _____________________________________________________________________________________
 `);
     })
 );
-
+ 
 const app = express();
 
 app.use(bodyParser.json());
