0ad49942c0813102927dd250b976a57e3b68a553
Claudio De Angelis <claudio.deangelis@cloud-iq.com>
2018-12-20 21:21:41 +1100

    fixed shebang

--- src/service.js
+++ src/service.js
@@ -1,4 +1,5 @@
-// #!/usr/bin/env node
+#!/usr/bin/env node
+
 const express = require('express');
 const { printConsole } = require('./utils/consoleLog');
 const { createHttpsServer, installSSLKey } = require('./https/httpsServer');
