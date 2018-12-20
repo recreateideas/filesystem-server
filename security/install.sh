7a026d0699afd0918b3002e873484181c1fdab3a
Claudio De Angelis <claudio.deangelis@cloud-iq.com>
2018-12-20 22:41:43 +1100

    install

--- security/install.sh
+++ security/install.sh
@@ -1,2 +1,12 @@
 openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout cert.key -out cert.pem -config req.cnf -sha256;
-printf "Installed SSL key and certificate. They are valid for 10 years.\n"
+printf "
+#############################################################################
+##                                                                         ##
+##                           !!  ATTENTION  !!                             ##
+##                                                                         ##
+##  We have installed SSL key and certificate to use with https protocol.  ##
+##                      These are valid for 10 years.                      ##
+##    To generate new ones run the 'postinstall' script in package.json.   ##
+##                                                                         ##
+#############################################################################\n\n
+"
